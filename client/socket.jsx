51d0c8bd027e361d104d9b743a22b2e3b7d2bf64
Vincent Li <35306435+vinrocksuper@users.noreply.github.com>
2022-11-20 17:40:13 -0500

    updated message create form with new styles

--- client/socket.jsx
+++ client/socket.jsx
@@ -1,53 +1,71 @@
 const socket = io();
 
 const handleEditbox = () => {
-  const editForm = document.getElementById('editForm');
-  const editBox = document.getElementById('editbox');
-  const channelSelect = document.getElementById('channelSelect');
+    const editForm = document.getElementById('editForm');
+    const editBox = document.getElementById('editbox');
+    const channelSelect = document.getElementById('channelSelect');
 
-  editForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    editForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    if (editBox.value) {
-      const data = {
-        message: editBox.value,
-        channel: channelSelect.value,
-      };
-      socket.emit('chat message', data);
-      editBox.value = '';
-    }
-  });
+        if (editBox.value) {
+            const data = {
+                message: editBox.value,
+                channel: channelSelect.value,
+            };
+            socket.emit('chat message', data);
+            editBox.value = '';
+        }
+    });
 };
 
 const displayMessage = (msg) => {
-  const messageDiv = document.createElement('div');
-  messageDiv.innerText = msg;
-  document.getElementById('messages').appendChild(messageDiv);
+    const messageDiv = document.createElement('div');
+    messageDiv.innerText = msg;
+    document.getElementById('messages').appendChild(messageDiv);
 };
 
 const handleChannelSelect = () => {
-  const channelSelect = document.getElementById('channelSelect');
-  const messages = document.getElementById('messages');
+    const channelSelect = document.getElementById('channelSelect');
+    const messages = document.getElementById('messages');
 
-  channelSelect.addEventListener('change', () => {
-    messages.innerHTML = '';
-    switch (channelSelect.value) {
-      case 'meme':
-        socket.off('general'); // unsubscribes from the event
-        socket.on('meme', displayMessage);
-        break;
-      default:
-        socket.off('meme'); // unsubscribes from the event
-        socket.on('general', displayMessage);
-        break;
-    }
-  });
+    channelSelect.addEventListener('change', () => {
+        messages.innerHTML = '';
+        switch (channelSelect.value) {
+            case 'meme':
+                socket.off('general'); // unsubscribes from the event
+                socket.on('meme', displayMessage);
+                break;
+            default:
+                socket.off('meme'); // unsubscribes from the event
+                socket.on('general', displayMessage);
+                break;
+        }
+    });
 };
 
+const CreateMessage = (props) => {
+    return (
+        <form id="editForm">
+            <textarea className="textarea is-medium" id="editbox" type="text" />
+            <br />
+            <div className="is-flex is-flex-direction-row-reverse">
+                <input className='button is-medium ml-2' type="submit" />
+                <select className="select is-medium ml-3" id="channelSelect">
+                    <option value="general">General</option>
+                    <option value="meme">Meme</option>
+                </select>
+            </div>
+
+        </form>
+    );
+}
+
 const init = () => {
-  handleEditbox();
-  socket.on('general', displayMessage);
-  handleChannelSelect();
+    ReactDOM.render(<CreateMessage />, document.getElementById('createMessage'));
+    handleEditbox();
+    socket.on('general', displayMessage);
+    handleChannelSelect();
 };
 
 window.onload = init;
